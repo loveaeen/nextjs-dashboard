ecec984517509ca08194f216f58918b650dcbed1
loveaeen <1625630746@qq.com>
2024-11-26 17:38:21 +0800

    Add metadata for my website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,29 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
